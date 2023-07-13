3aa74115680e30460ec334cce4e779149260d54c
ax-va <vasiliev.informatik@t-online.de>
2023-07-13 22:42:48 +0200

    update

--- project/js-sandpit/script-06-numbers.js
+++ project/js-sandpit/script-06-numbers.js
@@ -1,5 +1,5 @@
 let x = 3.45;
 console.log(x);  // 3.5
-console.log(typeof(x));  // number  // 64-bit floats
+console.log(typeof(x));  // number  // 64-bit float
 console.log(parseInt(x));  // 3
-console.log(typeof(parseInt(x)));  // number  // 64-bit floats
+console.log(typeof(parseInt(x)));  // number  // 64-bit float
