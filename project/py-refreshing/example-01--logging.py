cba2850217b645006cb1125482e2ff91ccd17fc5
ax-va <vasiliev.informatik@t-online.de>
2023-09-20 18:17:28 +0200

    update

--- project/py-refreshing/example-01--logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-# Creates a logger with the name of this module
-logger = logging.getLogger(__name__)
-
-logger.debug('Some useful debugging output')
-logger.info('Some general information')
-# Setting the logging level to 'debug' provides the most detailed information available
-logging.basicConfig(level=logging.DEBUG)
