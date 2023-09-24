cc27b2acb4a0e4785fdda6aea3c355ff10dfe280
Alexander Vasiliev <85578981+ax-va@users.noreply.github.com>
2023-09-24 09:24:53 +0200

    Update example-11--fetch.js

--- project/js-sandbox/example-11--fetch.js
+++ project/js-sandbox/example-11--fetch.js
@@ -1,4 +1,4 @@
-//  Fetch a dataset from the network, based on its URL,
+// Fetch a dataset from the network, based on its URL,
 // in the "static/data" directory
 fetch('/static/data/nobel_winners.json')
   .then(function(response) {
