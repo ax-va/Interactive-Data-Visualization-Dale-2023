10f4e7349b7d7c33eb785ef6cac001f18219dad1
ax-va <vasiliev.informatik@t-online.de>
2024-01-07 20:32:43 +0100

    update

--- project/sandbox/plotly-viz/native-js-charts/script-1.js
+++ project/sandbox/plotly-viz/native-js-charts/script-1.js
@@ -8,21 +8,23 @@ dataJSON.then(function (data) {
 
     console.log("Genders and award ages:", data);
 
-    let traces = [{
-        type: 'violin',
-        x: data.map(d => d.gender),
-        y: data.map(d => d.award_age),
-        points: 'none',
-        box: {
-            visible: true
-        },
-        line: {
-            color: 'green',
-        },
-        meanline: {
-            visible: true
-        },
-    }];
+    let traces = [
+        {
+            type: 'violin',
+            x: data.map(d => d.gender),
+            y: data.map(d => d.award_age),
+            points: 'none',
+            box: {
+                visible: true
+            },
+            line: {
+                color: 'green',
+            },
+            meanline: {
+                visible: true
+            },
+        }
+    ];
     
     let layout = {
         title: "Nobel Prize Violin Plot",
