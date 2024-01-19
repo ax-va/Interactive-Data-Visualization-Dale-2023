85ad7db4f2219b715eb0ae221b2f39bdb13361a2
ax-va <vasiliev.informatik@t-online.de>
2024-01-19 22:13:55 +0100

    update

--- project/sandbox/d3-examples/example-1--select--selectAll/index.js
+++ project/sandbox/d3-examples/example-1--select--selectAll/index.js
@@ -27,14 +27,42 @@ d3.select('#title')
 
 // Get values
 // Press Ctrl+Shift+J to open the Console tab in Google Chrome to see the logs
-console.log('d3.select("#barM").attr("id"):', d3.select("#barM").attr("id")) // barM
-console.log('d3.select("#barM").style("fill"):', d3.select("#barM").style("fill")) // red
-console.log('d3.select("#barM").classed("highlight"):', d3.select("#barM").classed("highlight")) // true
-console.log('d3.select("#barM").property("nodeName"):', d3.select("#barM").property("nodeName")) // rect
-console.log('d3.select("#title").attr("id"):', d3.select("#title").attr("id")) // title
-console.log('d3.select("#title").style("font-family"):', d3.select("#title").style("font-family")) // sant-serif
-console.log('d3.select("#title").text():', d3.select("#title").text()) // D3 select() and selectAll() example
-console.log('d3.select("#barchart").html():', d3.select("#barchart").html())
-//       <rect id="barL" class="bar" x="0" y="20" height="100" width="50" style="fill:rgb(100,200,200"></rect>
-//       <rect id="barM" class="bar highlight" x="55" y="30" height="50px" width="50" style="fill: red;"></rect>
-//       <rect id="barR" class="bar" x="110" y="40" height="100" width="50" style="fill:rgb(100,200,200"></rect>
+console.log('d3.select("#barM").attr("id"):', d3.select("#barM").attr("id")); // barM
+console.log('d3.select("#barM").style("fill"):', d3.select("#barM").style("fill")); // red
+console.log('d3.select("#barM").classed("highlight"):', d3.select("#barM").classed("highlight")); // true
+console.log('d3.select("#barM").property("nodeName"):', d3.select("#barM").property("nodeName")); // rect
+console.log('d3.select("#title").attr("id"):', d3.select("#title").attr("id")); // title
+console.log('d3.select("#title").style("font-family"):', d3.select("#title").style("font-family")); // sant-serif
+console.log('d3.select("#title").text():', d3.select("#title").text()); // D3 select() and selectAll() example
+console.log('d3.select("#barchart").html():', d3.select("#barchart").html());
+/*
+      <rect id="barL" class="bar" x="0" y="20" height="100" width="50" style="fill:rgb(100,200,200"></rect>
+      <rect id="barM" class="bar highlight" x="55" y="30" height="50px" width="50" style="fill: red;"></rect>
+      <rect id="barR" class="bar" x="110" y="40" height="100" width="50" style="fill:rgb(100,200,200"></rect>
+*/
+
+let ele = d3.select("#barchart");
+// Get DOMRect
+let bRect = ele.node().getBoundingClientRect();
+console.log('bRect:', bRect);
+/*
+DOMRect {x: 8, y: 67.8125, width: 300, height: 150, top: 67.8125, …}
+  bottom: 217.8125
+  height: 150
+  left: 8
+  right: 308
+  top: 67.8125
+  width: 300
+  x: 8
+  y: 67.8125
+*/
+// Get SVGRect
+let bBox = ele.node().getBBox();
+console.log('bBox:', bBox);
+/*
+SVGRect {x: 0, y: 20, width: 160, height: 120}
+  height: 120
+  width: 160
+  x: 0
+  y: 20
+*/
