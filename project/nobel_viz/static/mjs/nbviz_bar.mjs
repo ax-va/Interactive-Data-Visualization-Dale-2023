5fffdd0262b7f671896b43ef33d520506ea8e95f
ax-va <vasiliev.informatik@t-online.de>
2024-01-21 13:54:23 +0100

    update

---
+++ project/nobel_viz/static/mjs/nbviz_bar.mjs
@@ -0,0 +1,6 @@
+import nbviz from './nbviz_core.mjs';
+// Once this module is first imported, callbacks are updated
+nbviz.callbacks.push(() => {
+    let data = nbviz.getCountryData();
+    updateBarChart(data);
+});
