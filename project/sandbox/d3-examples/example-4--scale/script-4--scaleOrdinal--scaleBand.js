7551578f29f4d4a740a6a3a78114a38d211f2986
ax-va <vasiliev.informatik@t-online.de>
2024-01-24 18:55:03 +0100

    update

--- project/sandbox/d3-examples/example-4--scale/script-4--scaleOrdinal--scaleBand.js
+++ project/sandbox/d3-examples/example-4--scale/script-4--scaleOrdinal--scaleBand.js
@@ -43,3 +43,31 @@ console.log("oScale(1):", oScale(1)); // oScale(1): 10
 console.log("oScale(2):", oScale(2)); // oScale(2): 55
 console.log("oScale.bandwidth():", oScale.bandwidth()); // oScale.bandwidth(): 36
 // | 10 | 36 | 9 | 36 | 9 |
+
+// Read JSON and then handle the promise
+d3.json("data/nobel-winners.json").then((nobelWinners) => {
+  // Handle the promise
+  console.log("Nobel winners:", nobelWinners);
+  /*
+  nobelWinners is an array of dictionaries:
+    [
+      {key: 'United States', value: '336'}, // The value is a string
+      {key: 'United Kingdom', value: 98},
+      {key: 'Germany', value: 79},
+      {key: 'France', value: 60},
+      {key: 'Sweden', value: 29},
+      {key: 'Switzerland', value: 23},
+      {key: 'Japan', value: 21},
+      {key: 'Russia', value: 19},
+      {key: 'Netherlands', value: 17},
+      {key: 'Austria', value: 14},
+    ]
+  */
+
+  let width = 600;
+
+  let xScale = d3.scaleBand()
+    .domain(d3.range(nobelWinners.length))
+    .rangeRound([0, width])
+    .padding(0.1);
+});
